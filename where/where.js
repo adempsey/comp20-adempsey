64a8778dd73a309d9c49d3c04c35c8e8944bdabb
adempsey <amd0331@gmail.com>
2013-02-28 02:16:31 -0500

    added infowindows for mbta stations

--- where/where.js
+++ where/where.js
@@ -30,17 +30,26 @@ function renderMap() {
 	
 	/* create red line markers */
 	redLineData();
+	stopMarkers = new Array();
 	for (i in stopLoc) {
-		stopCoords = new google.maps.LatLng(stopLoc[i].lat, stopLoc[i].long);
-		stopMarker = new google.maps.Marker({position: stopCoords, title: stopLoc[i].name});
-		stopMarker.setMap(map);
+		var stop = stopLoc[i];
+		stopCoords = new google.maps.LatLng(stop.lat, stop.long);
+		stop = new google.maps.Marker({position: stopCoords, title: stop.name});
+		stop.setMap(map);
+		
+		/* create info window for station location markers */
+		var infowindow = new google.maps.InfoWindow();
+		google.maps.event.addListener(stop, 'click', function() {
+			infowindow.setContent("<div class='stationname'>" + this.title + "</div>");
+			infowindow.open(map, this);
+		});
 	}
 	
-	/* create info window for marker */
+	/* create info window for current location marker */
 	google.maps.event.addListener(marker, 'click', function() {
-			infowindow.setContent(marker.title);
-			infowindow.open(map, marker);
-		});
+		infowindow.setContent(marker.title);
+		infowindow.open(map, marker);
+	});
 }
 
 function redLineData() {
@@ -85,8 +94,10 @@ function redLineData() {
 }
 
 function initRedLine() {
+	/* static data for mbta station coordinates */
 	locdata = "ALEWIFE,Alewife Station,42.395428,-71.142483,DAVIS,Davis Station,42.39674,-71.121815,PORTER,Porter Square Station,42.3884,-71.119149,HARVARD,Harvard Square Station,42.373362,-71.118956,CENTRAL,Central Square Station,42.365486,-71.103802,KENDALL,Kendall/MIT Station,42.36249079,-71.08617653,CHARLES MGH,Charles/MGH Station,42.361166,-71.070628,PARK,Park St. Station,42.35639457,-71.0624242,DOWNTOWN CROSSING,Downtown Crossing Station,42.355518,-71.060225,SOUTH STATION,South Station,42.352271,-71.055242,BROADWAY,Broadway Station,42.342622,-71.056967,ANDREW,Andrew Station,42.330154,-71.057655,JFK,JFK/UMass Station,42.320685,-71.052391,SAVIN HILL,Savin Hill Station,42.31129,-71.053331,FIELDS CORNER,Fields Corner Station,42.300093,-71.061667,SHAWMUT,Shawmut Station,42.29312583,-71.06573796,ASHMONT,Ashmont Station,42.284652,-71.064489,NORTH QUINCY,North Quincy Station,42.275275,-71.029583,WOLLASTON,Wollaston Station,42.2665139,-71.0203369,QUINCY CENTER,Quincy Center Station,42.251809,-71.005409,QUINCY ADAMS,Quincy Adams Station,42.233391,-71.007153,BRAINTREE,Braintree Station,42.2078543,-71.0011385";
 
+	/* parse string into array */
 	counter = 0;
 	index = 0;
 	locarr = locdata.split(",");
