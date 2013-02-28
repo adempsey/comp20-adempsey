7fc3aeb7423690ec639f057e20caf18fa07483e2
adempsey <amd0331@gmail.com>
2013-02-28 03:22:41 -0500

    added polyline

--- where/where.js
+++ where/where.js
@@ -25,8 +25,14 @@ function renderMap() {
 	
 	/* create current location marker */
 	currentLoc = new google.maps.LatLng(lat, long);
-	marker = new google.maps.Marker({position: currentLoc, title: "I am here at " + lat.toFixed(3) + ", " + long.toFixed(3)});
+	marker = new google.maps.Marker({position: currentLoc, title: "You are here at " + lat.toFixed(3) + ", " + long.toFixed(3)});
 	marker.setMap(map);
+		
+	/* create info window for current location marker */
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.setContent(marker.title);
+		infowindow.open(map, marker);
+	});
 	
 	/* create red line markers */
 	redLineData();
@@ -45,11 +51,29 @@ function renderMap() {
 		});
 	}
 	
-	/* create info window for current location marker */
-	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent(marker.title);
-		infowindow.open(map, marker);
+	/* draw polyline between stops */
+	var stationCoor = new Array();
+	j = 0;
+	for (i = 0; i < 17; i++) {
+		stationCoor[j] = new google.maps.LatLng(stopLoc[i].lat, stopLoc[i].long);
+		j++;
+	}
+	for (i = 15; i > 11; i--) {
+		stationCoor[j] = new google.maps.LatLng(stopLoc[i].lat, stopLoc[i].long);
+		j++;
+	}
+	
+	for (i = 17; i < 22; i++) {
+		stationCoor[j] = new google.maps.LatLng(stopLoc[i].lat, stopLoc[i].long);
+		j++;
+	}
+	line = new google.maps.Polyline({
+		path: stationCoor,
+		strokeColor: "#EE0000",
+		strokeOpacity: 1,
+		strokeWeight: 2
 	});
+	line.setMap(map);
 }
 
 function redLineData() {
@@ -95,7 +119,7 @@ function redLineData() {
 
 function initRedLine() {
 	/* static data for mbta station coordinates */
-	locdata = "ALEWIFE,Alewife Station,42.395428,-71.142483,DAVIS,Davis Station,42.39674,-71.121815,PORTER,Porter Square Station,42.3884,-71.119149,HARVARD,Harvard Square Station,42.373362,-71.118956,CENTRAL,Central Square Station,42.365486,-71.103802,KENDALL,Kendall/MIT Station,42.36249079,-71.08617653,CHARLES MGH,Charles/MGH Station,42.361166,-71.070628,PARK,Park St. Station,42.35639457,-71.0624242,DOWNTOWN CROSSING,Downtown Crossing Station,42.355518,-71.060225,SOUTH STATION,South Station,42.352271,-71.055242,BROADWAY,Broadway Station,42.342622,-71.056967,ANDREW,Andrew Station,42.330154,-71.057655,JFK,JFK/UMass Station,42.320685,-71.052391,SAVIN HILL,Savin Hill Station,42.31129,-71.053331,FIELDS CORNER,Fields Corner Station,42.300093,-71.061667,SHAWMUT,Shawmut Station,42.29312583,-71.06573796,ASHMONT,Ashmont Station,42.284652,-71.064489,NORTH QUINCY,North Quincy Station,42.275275,-71.029583,WOLLASTON,Wollaston Station,42.2665139,-71.0203369,QUINCY CENTER,Quincy Center Station,42.251809,-71.005409,QUINCY ADAMS,Quincy Adams Station,42.233391,-71.007153,BRAINTREE,Braintree Station,42.2078543,-71.0011385";
+	locdata = "ALEWIFE,Alewife,42.395428,-71.142483,DAVIS,Davis,42.39674,-71.121815,PORTER,Porter Square,42.3884,-71.119149,HARVARD,Harvard Square,42.373362,-71.118956,CENTRAL,Central Square,42.365486,-71.103802,KENDALL,Kendall/MIT,42.36249079,-71.08617653,CHARLES MGH,Charles/MGH,42.361166,-71.070628,PARK,Park St.,42.35639457,-71.0624242,DOWNTOWN CROSSING,Downtown Crossing,42.355518,-71.060225,SOUTH STATION,South Station,42.352271,-71.055242,BROADWAY,Broadway,42.342622,-71.056967,ANDREW,Andrew,42.330154,-71.057655,JFK,JFK/UMass,42.320685,-71.052391,SAVIN HILL,Savin Hill,42.31129,-71.053331,FIELDS CORNER,Fields Corner,42.300093,-71.061667,SHAWMUT,Shawmut,42.29312583,-71.06573796,ASHMONT,Ashmont,42.284652,-71.064489,NORTH QUINCY,North Quincy,42.275275,-71.029583,WOLLASTON,Wollaston,42.2665139,-71.0203369,QUINCY CENTER,Quincy Center,42.251809,-71.005409,QUINCY ADAMS,Quincy Adams,42.233391,-71.007153,BRAINTREE,Braintree,42.2078543,-71.0011385";
 
 	/* parse string into array */
 	counter = 0;
