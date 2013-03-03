ef2a93152ec2450cf489b690c8cdd3aea86a8852
adempsey <amd0331@gmail.com>
2013-03-03 16:24:49 -0500

    added loading window for mbta data

--- where/where.js
+++ where/where.js
@@ -16,7 +16,7 @@ function getMap() {
 		alert("Your browser does not support geolocation.");
 	}
 }
-//add function for putting AJAX data on map so it loads more quickly
+
 function renderMap() {
 	/* initialize map */
 	var mapOptions = {
@@ -25,21 +25,21 @@ function renderMap() {
 	  mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-	
+
 	/* create current location marker */
 	currentLoc = new google.maps.LatLng(lat, long);
 	marker     = new google.maps.Marker({position: currentLoc, title: "You are here at " + lat.toFixed(3) + ", " + long.toFixed(3)});
 	marker.setMap(map);
-		
+
 	/* create info window for current location marker */
 	google.maps.event.addListener(marker, 'click', function() {
 		infowindow.setContent(marker.title);
 		infowindow.open(map, marker);
 	});
-	
+
 	/* create red line markers */
 	initRedLine();
-	
+
 	/* retrieve waldo and carmen */
 	initPeople();
 }
@@ -61,12 +61,13 @@ function getActiveRedLineData() {
 		}
 	}
 	if (redLineRequest == null) alert("Error creating request object --Ajax not supported?");
-	
+
 	/* parse json data into info */
 	redLineRequest.onreadystatechange = function() {
 		redLineRequest.readystate;
 		if (redLineRequest.readyState == 4) {
 			try {
+				document.getElementById("loader").style.visibility = 'hidden';
 				trainInfo = JSON.parse(redLineRequest.responseText);
 				/* adds active train data to each stop */
 				for (i in trainInfo) {
@@ -83,6 +84,10 @@ function getActiveRedLineData() {
 				redLineRequest.abort();
 				document.getElementById("people_data").innerHTML+="<span class='error'>Error: Could not fetch MBTA data</span>";
 			}	
+		} else {
+			var loader = document.getElementById("loader");
+			loader.style.visibility = 'visible';
+			loader.innerHTML = "loading MBTA data...";
 		}
 	}
 	redLineRequest.open("GET", "http://mbtamap-cedar.herokuapp.com/mapper/redline.json", true);
@@ -91,7 +96,7 @@ function getActiveRedLineData() {
 
 function initRedLine() {
 	stopLoc = new Array();
-	
+
 	/* static data for mbta station coordinates */
 	locdata = "RALE,Alewife,42.395428,-71.142483,RDAV,Davis,42.39674,-71.121815,RPOR,Porter Square,42.3884,-71.119149,RHAR,Harvard Square,42.373362,-71.118956,RCEN,Central Square,42.365486,-71.103802,RKEN,Kendall/MIT,42.36249079,-71.08617653,RMGH,Charles/MGH,42.361166,-71.070628,RPRK,Park St.,42.35639457,-71.0624242,RDTC,Downtown Crossing,42.355518,-71.060225,RSOU,South Station,42.352271,-71.055242,RBRO,Broadway,42.342622,-71.056967,RAND,Andrew,42.330154,-71.057655,RJFK,JFK/UMass,42.320685,-71.052391,RSAV,Savin Hill,42.31129,-71.053331,RFIE,Fields Corner,42.300093,-71.061667,RSHA,Shawmut,42.29312583,-71.06573796,RASH,Ashmont,42.284652,-71.064489,RNQU,North Quincy,42.275275,-71.029583,RWOL,Wollaston,42.2665139,-71.0203369,RQUC,Quincy Center,42.251809,-71.005409,RQUA,Quincy Adams,42.233391,-71.007153,RBRA,Braintree,42.2078543,-71.0011385";
 
@@ -131,12 +136,12 @@ function renderRedLine() {
 	for (i in stopLoc) {
 		var stop = stopLoc[i];
 		stopCoords = new google.maps.LatLng(stop.lat, stop.long);
-		
+
 		northTrains = ""; southTrains = "";
 		for (j in stopLoc[i].nTrains) northTrains += "<li>Northbound: " + stopLoc[i].nTrains[j] + "</li>";
 		for (j in stopLoc[i].sTrains) southTrains += "<li>Southbound: " + stopLoc[i].sTrains[j] + "</li>";
 		arrivals = "<ul>" + northTrains + southTrains + "</ul>";
-		
+
 		stop = new google.maps.Marker({
 			position: stopCoords, 
 			   title: stop.name, 
@@ -145,7 +150,7 @@ function renderRedLine() {
 			   scale: .5
 		});
 		stop.setMap(map);
-		
+
 		/* create info window for station location markers */
 		var infowindow = new google.maps.InfoWindow();
 		google.maps.event.addListener(stop, 'click', function() {
@@ -153,15 +158,15 @@ function renderRedLine() {
 			infowindow.open(map, this);
 		});
 	}
-	
+
 	/* draw polyline between stops */
 	var stationCoor = new Array();
-	
+
 	j = 0;
 	for (i =  0; i < 17; i++) { stationCoor[j] = new google.maps.LatLng(stopLoc[i].lat, stopLoc[i].long); j++; }
 	for (i = 15; i > 11; i--) { stationCoor[j] = new google.maps.LatLng(stopLoc[i].lat, stopLoc[i].long); j++; }
 	for (i = 17; i < 22; i++) { stationCoor[j] = new google.maps.LatLng(stopLoc[i].lat, stopLoc[i].long); j++; }
-	
+
 	line = new google.maps.Polyline({
 		         path: stationCoor,
 		  strokeColor: "#EE0000",
@@ -173,7 +178,7 @@ function renderRedLine() {
 
 function initPeople() {
 	/* initiate waldo and carmen XMLHttpRequest */
-	
+
 	var peopleRequest;
 	try {
 		peopleRequest = new XMLHttpRequest();
@@ -189,7 +194,7 @@ function initPeople() {
 		}
 	}
 	if (peopleRequest == null) alert("Error creating request object --Ajax not supported?");
-	
+
 	/* parse json data into peopleInfo */
 	peopleRequest.onreadystatechange = function() {
 		if (peopleRequest.readyState == 4) {
@@ -202,7 +207,7 @@ function initPeople() {
 				}	
 			}
 		}
-	peopleRequest.open("GET", "http://messagehub.herokuapp.com/a3.json", false);
+	peopleRequest.open("GET", "http://messagehub.herokuapp.com/a3.json", true);
 	peopleRequest.send();
 }
 
@@ -217,7 +222,7 @@ function renderPeople() {
 			    icon: peopleInfo[i]['name'] == "Waldo" ? "waldo.png" : "carmen.png"
 		});
 		personMarker.setMap(map);
-		
+
 		/* create info window for waldo and carmen */
 		var infowindow = new google.maps.InfoWindow();
 		google.maps.event.addListener(personMarker, 'click', function() {
@@ -225,7 +230,7 @@ function renderPeople() {
 			infowindow.open(map, this);
 		});
 	}
-	
+
 	/* get distance to waldo and carmen --waldo's first */
 	for (i in peopleInfo) {
 		document.getElementById("people_data").innerHTML += "<p>Distance to " + peopleInfo[i]['name'] + " is " + distance(i) + "</p>";
@@ -240,10 +245,9 @@ function distance(p) {
 	var dLat = parseFloat((lat - personLat), 10) * 3.141592653589 / 180;
 	var dLon = parseFloat((long - personLong), 10) * 3.141592653589 / 180;
 	var userLat = parseFloat(lat, 10) * 3.141592653589 / 180;
-	
+
 	var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(userLat) * Math.cos(personLat);
 	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 	var d = 6371 * c;
 	return d.toFixed(3);
 }
-
