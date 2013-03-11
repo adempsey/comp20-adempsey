7dc7683af470c05fc76a6f8bcd01c7ef3cb19853
adempsey <amd0331@gmail.com>
2013-03-11 03:50:09 -0400

    fixed spacing of vehicles and logs

--- frogger/game.js
+++ frogger/game.js
@@ -12,7 +12,7 @@ function start_game() {
 }
 
 function init(game) {
-	game.frog_x = 185;
+	game.frog_x = 190;
 	game.frog_y = 490;
 	game.lives = 5;
 	game.over = false;
@@ -122,8 +122,8 @@ function draw() {
 	game.fillRect(0,0,399,300);
 	
 	game.drawImage(sprites,0,0  ,399,110,0,0  ,399,110); //title and grass
-	game.drawImage(sprites,0,117,399,37 ,0,270,399,37 ); //top road
-	game.drawImage(sprites,0,117,399,37 ,0,480,399,37 ); //bottom road
+	game.drawImage(sprites,0,117,399,37 ,0,293,399,37 ); //top of road
+	game.drawImage(sprites,0,117,399,37 ,0,480,399,37 ); //bottom of road
 	
 	/* medium log */
 	game.drawImage(sprites,0,197,121,22,game.med_log_loc-50 ,115,121,22);
@@ -136,55 +136,61 @@ function draw() {
 	game.drawImage(sprites,0,229,90,22,game.short_log_rev_loc-400,145,82,22);
 	
 	/* long log */
-	game.drawImage(sprites,0,165,182,22,game.long_log_loc    ,175,189,22);
-	game.drawImage(sprites,0,165,182,22,game.long_log_loc-242,175,189,22);
-	game.drawImage(sprites,0,165,182,22,game.long_log_loc-484,175,189,22);
+	game.drawImage(sprites,0,165,182,22,game.long_log_loc    ,176,189,22);
+	game.drawImage(sprites,0,165,182,22,game.long_log_loc-242,176,189,22);
+	game.drawImage(sprites,0,165,182,22,game.long_log_loc-484,176,189,22);
 	
 	/* short log */
-	game.drawImage(sprites,0,229,90,22,game.short_log_loc    ,210,82,22);
-	game.drawImage(sprites,0,229,90,22,game.short_log_loc-200,210,82,22);
-	game.drawImage(sprites,0,229,90,22,game.short_log_loc-400,210,82,22);
+	game.drawImage(sprites,0,229,90,22,game.short_log_loc    ,208,82,22);
+	game.drawImage(sprites,0,229,90,22,game.short_log_loc-200,208,82,22);
+	game.drawImage(sprites,0,229,90,22,game.short_log_loc-400,208,82,22);
 	
-	/* medium reverse log */
-	game.drawImage(sprites,0,197,121,22,game.med_log_loc    ,240,121,22);
-	game.drawImage(sprites,0,197,121,22,game.med_log_loc-242,240,121,22);
-	game.drawImage(sprites,0,197,121,22,game.med_log_loc-484,240,121,22);	
+	/* medium log 2 */
+	game.drawImage(sprites,0,197,121,22,game.med_log_loc    ,238,121,22);
+	game.drawImage(sprites,0,197,121,22,game.med_log_loc-242,238,121,22);
+	game.drawImage(sprites,0,197,121,22,game.med_log_loc-484,238,121,22);
+	
+	/* short log 2 */	
+	
+	game.drawImage(sprites,0,229,90,22,game.short_log_rev_loc    ,270,82,22);
+	game.drawImage(sprites,0,229,90,22,game.short_log_rev_loc-200,270,82,22);
+	game.drawImage(sprites,0,229,90,22,game.short_log_rev_loc-400,270,82,22);
 	
 	/* pink car */
-	game.drawImage(sprites,10,268,28,21,game.pink_car_loc    ,385,28,21);
-	game.drawImage(sprites,10,268,28,21,game.pink_car_loc+100,385,28,21);
-	game.drawImage(sprites,10,268,28,21,game.pink_car_loc+200,385,28,21);
-	game.drawImage(sprites,10,268,28,21,game.pink_car_loc+500,385,28,21);
-	game.drawImage(sprites,10,268,28,21,game.pink_car_loc+600,385,28,21);
+	game.drawImage(sprites,10,268,28,21,game.pink_car_loc    ,395,28,21);
+	game.drawImage(sprites,10,268,28,21,game.pink_car_loc+100,395,28,21);
+	game.drawImage(sprites,10,268,28,21,game.pink_car_loc+200,395,28,21);
+	game.drawImage(sprites,10,268,28,21,game.pink_car_loc+500,395,28,21);
+	game.drawImage(sprites,10,268,28,21,game.pink_car_loc+600,395,28,21);
 	
 	/* yellow car */
-	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc    ,460,28,21);
-	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+100,460,28,21);
-	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+200,460,28,21);
-	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+500,460,28,21);
-	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+600,460,28,21);
-	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+700,460,28,21);
+	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc    ,456,28,21);
+	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+100,456,28,21);
+	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+200,456,28,21);
+	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+500,456,28,21);
+	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+600,456,28,21);
+	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+700,456,28,21);
 	
 	/* truck */
-	game.drawImage(sprites,106,300,46,20,game.truck_loc    ,305,46,20);
-	game.drawImage(sprites,106,300,46,20,game.truck_loc+150,305,46,20);
-	game.drawImage(sprites,106,300,46,20,game.truck_loc+400,305,46,20);
-	game.drawImage(sprites,106,300,46,20,game.truck_loc+550,305,46,20);
+	game.drawImage(sprites,106,300,46,20,game.truck_loc    ,330,46,20);
+	game.drawImage(sprites,106,300,46,20,game.truck_loc+150,330,46,20);
+	game.drawImage(sprites,106,300,46,20,game.truck_loc+400,330,46,20);
+	game.drawImage(sprites,106,300,46,20,game.truck_loc+550,330,46,20);
 	
 	/* race car */
-	game.drawImage(sprites,47,266,27,25,game.race_car_loc    ,420,27,25);
-	game.drawImage(sprites,47,266,27,25,game.race_car_loc-100,420,27,25);
-	game.drawImage(sprites,47,266,27,25,game.race_car_loc-200,420,27,25);
-	game.drawImage(sprites,47,266,27,25,game.race_car_loc-500,420,27,25);
-	game.drawImage(sprites,47,266,27,25,game.race_car_loc-600,420,27,25);
+	game.drawImage(sprites,47,266,27,25,game.race_car_loc    ,424,27,25);
+	game.drawImage(sprites,47,266,27,25,game.race_car_loc-100,424,27,25);
+	game.drawImage(sprites,47,266,27,25,game.race_car_loc-200,424,27,25);
+	game.drawImage(sprites,47,266,27,25,game.race_car_loc-500,424,27,25);
+	game.drawImage(sprites,47,266,27,25,game.race_car_loc-600,424,27,25);
 	
 	/* upper yellow car */
-	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+120,345,28,21);
-	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+220,345,28,21);
-	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+320,345,28,21);
-	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+620,345,28,21);
-	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+720,345,28,21);
-	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+820,345,28,21);
+	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+120,363,28,21);
+	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+220,363,28,21);
+	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+320,363,28,21);
+	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+620,363,28,21);
+	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+720,363,28,21);
+	game.drawImage(sprites,81,265,28,25,game.yellow_car_loc+820,363,28,21);
 	
 	/* frog */
 	game.drawImage(sprites,10,370,25,20,game.frog_x,game.frog_y,25,20);
